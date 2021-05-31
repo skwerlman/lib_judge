5badad8b207a9e165ece3951b3615d72172fdad0
skwerlman <skwerlman@users.noreply.github.com>
2021-05-31 18:33:53 -0400

    huh, easiest 100% coverage ive ever seen

--- test/lib_judge_tokenizer_test.exs
+++ test/lib_judge_tokenizer_test.exs
@@ -1,4 +1,12 @@
 defmodule LibJudgeTokenizerTest do
   use ExUnit.Case, async: true
   doctest LibJudge.Tokenizer
+
+  describe "tokenizer" do
+    @raw_text LibJudge.get!("20210224", false)
+
+    test "can read rules from a string" do
+      assert is_list(LibJudge.tokenize(@raw_text))
+    end
+  end
 end
