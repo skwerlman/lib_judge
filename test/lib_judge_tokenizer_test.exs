294541649cde681b759a191b7c46da55523a8423
skwerlman <skwerlman@users.noreply.github.com>
2021-05-31 19:19:13 -0400

    do a little validation on the tokens returned

--- test/lib_judge_tokenizer_test.exs
+++ test/lib_judge_tokenizer_test.exs
@@ -4,9 +4,26 @@ defmodule LibJudgeTokenizerTest do
 
   describe "tokenizer" do
     @raw_text LibJudge.get!("20210224", false)
+    @token_kinds [
+      :title,
+      :effective_date,
+      :intro,
+      :contents,
+      :rule,
+      :glossary
+    ]
 
-    test "can read rules from a string" do
-      assert is_list(LibJudge.tokenize(@raw_text))
+    test "returns a list of tokens" do
+      tokens = LibJudge.tokenize(@raw_text)
+      assert is_list(tokens)
+
+      assert Enum.reduce(tokens, true, fn
+               {kind, _token}, acc ->
+                 kind in @token_kinds and acc
+
+               _, _ ->
+                 false
+             end)
     end
   end
 end
