fe185a97c55ba040c936bada12c269481be3acec
skwerlman <skwerlman@users.noreply.github.com>
2021-05-31 17:32:34 -0400

    fix credo warning

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,10 +1,14 @@
-System.cmd("wget", [
-  "-t",
-  "10",
-  "-nc",
-  "-O",
-  "priv/data/MagicCompRules 20210224.txt",
-  "https://media.wizards.com/2021/downloads/MagicCompRules%2020210224.txt"
-])
+System.cmd(
+  "wget",
+  [
+    "-t",
+    "10",
+    "-nc",
+    "-O",
+    "priv/data/MagicCompRules 20210224.txt",
+    "https://media.wizards.com/2021/downloads/MagicCompRules%2020210224.txt"
+  ],
+  env: %{}
+)
 
 ExUnit.start()
