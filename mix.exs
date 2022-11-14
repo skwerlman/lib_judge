fd1e4127b03d893e184d8758e96475caa8bddacb
skwerlman <skwerlman@users.noreply.github.com>
2022-11-14 08:44:17 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule ArchiveChallenges.MixProject do
   @moduledoc false
   use Mix.Project
 
-  @version "0.4.2"
+  @version "0.4.3"
   @repo "https://github.com/skwerlman/lib_judge"
 
   def project do
